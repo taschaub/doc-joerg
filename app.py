63c1a97e4e01ebaf48ef5337ad934250b309c8aa
taschaub <tim.aaron.schaub@gmail.com>
2024-10-02 12:06:09 +0000

    first database test

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
+# Konfiguration der Datenbank
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Definition des Modells
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f'<Project {self.name}>'
+
 @app.route('/')
 def home():
-    return 'Hallo, willkommen zum Dokumentenmanagement-System!'
+    projects = Project.query.all()
+    return render_template('home.html', projects=projects)
 
 if __name__ == '__main__':
     app.run(debug=True)
